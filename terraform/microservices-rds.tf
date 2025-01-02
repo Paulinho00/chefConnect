c4ed9bccad283e90a9031286b5529f91270f9439
Pawel Gryglewicz <pawel.gryglewicz09@gmail.com>
2025-01-02 16:34:41 +0100

    Rds for restaurant service

---
+++ terraform/microservices-rds.tf
@@ -0,0 +1,13 @@
+module "restaurant_service_db" {
+  source = "./rds_module"
+  username = "postgres"
+  password = "postgres"
+  db_name = "restaurant-db"
+  identifier = "restaurant-db"
+  vpc_security_group_ids = [aws_security_group.restaurant_db_security_group.id]
+  subnet_ids = [aws_subnet.main_first_subnet.id, aws_subnet.main_second_subnet.id]
+}
+
+output "restaurant_ednpoint" {
+  value = module.restaurant_service_db.rds_instance_endpoint
+}

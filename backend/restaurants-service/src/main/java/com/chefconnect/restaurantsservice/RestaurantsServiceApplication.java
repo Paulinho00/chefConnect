43a6cd1549d13e90c8cf8411d81e4378e33da19d
Jakub Wirwis <jakub.wirwis@tensquaregames.com>
2025-01-02 18:03:27 +0100

    add heathcheck endpoint to RestaurantService

--- backend/restaurants-service/src/main/java/com/chefconnect/restaurantsservice/RestaurantsServiceApplication.java
+++ backend/restaurants-service/src/main/java/com/chefconnect/restaurantsservice/RestaurantsServiceApplication.java
@@ -3,15 +3,12 @@ package com.chefconnect.restaurantsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
-@EnableMethodSecurity
 @EnableConfigurationProperties(EventQueuesProperties.class)
 public class RestaurantsServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestaurantsServiceApplication.class, args);
     }
-
 }
---
+++ backend/restaurants-service/src/main/java/com/chefconnect/restaurantsservice/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.chefconnect.restaurantsservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/health").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf().disable();
+
+        return http.build();
+    }
+}

101ff5bd28ff098c3f22c92fa0a911f6a95e9449
Jakub Wirwis <jakub.wirwis@tensquaregames.com>
2025-01-04 13:49:41 +0100

    add getRestaurantById method to api

--- backend/restaurants-service/src/main/java/com/chefconnect/restaurantsservice/controller/RestaurantController.java
+++ backend/restaurants-service/src/main/java/com/chefconnect/restaurantsservice/controller/RestaurantController.java
@@ -8,12 +8,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -28,5 +26,13 @@ public class RestaurantController {
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity<Collection<RestaurantDto>> getAllRestaurants() {
         return ResponseEntity.ok(restaurantQueryService.getAllRestaurants());
+    }
+
+    @GetMapping("/{restaurantId}")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<RestaurantDto> getRestaurantById(@PathVariable UUID restaurantId) {
+        return restaurantQueryService.get(restaurantId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
--- backend/restaurants-service/src/main/java/com/chefconnect/restaurantsservice/service/RestaurantQueryService.java
+++ backend/restaurants-service/src/main/java/com/chefconnect/restaurantsservice/service/RestaurantQueryService.java
@@ -8,12 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor(onConstructor_ = @Autowired, access = AccessLevel.PROTECTED)
 public class RestaurantQueryService {
 
     private final RestaurantRepository restaurantRepository;
+
+    public Optional<RestaurantDto> get(UUID restaurantId) {
+        return restaurantRepository.findById(restaurantId).map(RestaurantDto::fromEntity);
+    }
 
     public Collection<RestaurantDto> getAllRestaurants() {
         return restaurantRepository.findAll()

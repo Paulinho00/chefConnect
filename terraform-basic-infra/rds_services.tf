03ec1f7fe72f08e9338b4c8282fb57b922e27aac
Jakub Wirwis <jakub.wirwis@tensquaregames.com>
2025-01-21 19:51:21 +0100

    add Kubernetes .yml for analytics-service

--- terraform-basic-infra/rds_services.tf
+++ terraform-basic-infra/rds_services.tf
@@ -24,6 +24,16 @@ module "reservations_service_db" {
   subnet_ids = [aws_subnet.microservices_private_1.id, aws_subnet.microservices_private_2.id]
 }
 
+module "reservations_service_db" {
+  source = "./rds_module"
+  username = "postgres"
+  password = "postgres"
+  db_name = "analyticsDb"
+  identifier = "analytics-db"
+  vpc_id = aws_vpc.microservice_main.id
+  subnet_ids = [aws_subnet.microservices_private_1.id, aws_subnet.microservices_private_2.id]
+}
+
 output "reservations_ednpoint" {
   value="jdbc:postgresql://${module.reservations_service_db.rds_instance_endpoint}/${module.reservations_service_db.rds_db_name}"
 }
